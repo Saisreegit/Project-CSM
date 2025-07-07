fa30098442d5a27c9179106ff1eeb5228f08ec23
Saisreegit <saisreekemburu@gmail.com>
2025-07-07 15:34:35 +0530

    __init__.py

--- crms/__init__.py
+++ crms/__init__.py
@@ -1,6 +1,31 @@
-from flask import Blueprint
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from dotenv import load_dotenv
+import os
 
-crms_bp = Blueprint('crms', __name__, template_folder='templates', static_folder='static')
+db = SQLAlchemy()
+mail = Mail()
 
-from . import routes  # make sure routes.py exists
+def create_app():
+    load_dotenv()
+    
+    app = Flask(__name__)
+    app.secret_key = 'your_secret_key_here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root123@localhost:3307/crms_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+
+    db.init_app(app)
+    mail.init_app(app)
+
+    from .routes import crms_bp
+    app.register_blueprint(crms_bp)
+
+    return app
