42f58358f8a2a354ad764ca4995d743bc900cd04
nsandcy2 <sandeepboddu490@gmail.com>
2025-08-28 11:40:50 +0530

    integrated 0.1v of safe_ops

---
+++ safe_ops/__init__.py
@@ -0,0 +1,5 @@
+# from flask import Blueprint
+
+# fsrm_bp = Blueprint('safe_ops', __name__, template_folder='templates', static_folder='static')
+
+# from . import app  # This will import your fsrm/app.py where routes are defined
